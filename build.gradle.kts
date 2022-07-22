0b5a48cb22b3b8bcd5e668ce9426c7367e127865
seiko <seiko_des@outlook.com>
2022-07-22 21:05:26 +0800

    set publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("com.android.application").apply(false)
     id("com.android.library").apply(false)
     kotlin("android").apply(false)
     id("com.diffplug.spotless").version(Versions.spotless)
+    id("com.vanniktech.maven.publish").version("0.20.0").apply(false)
 }
 
 allprojects {
@@ -34,6 +38,41 @@ allprojects {
             target("**/*.java")
             targetExclude("$buildDir/**/*.java", "bin/**/*.java")
             // licenseHeaderFile(rootProject.file("spotless/license"))
+        }
+    }
+
+    group = "io.github.qdsfdhvh"
+    version = "1.0.7"
+
+    plugins.withId("com.vanniktech.maven.publish.base") {
+        @Suppress("UnstableApiUsage")
+        configure<MavenPublishBaseExtension> {
+            publishToMavenCentral(SonatypeHost.S01)
+            signAllPublications()
+            pom {
+                name.set("compose-imageLoader")
+                description.set("Compose ImageLoader.")
+                url.set("https://github.com/qdsfdhvh/compose-imageLoader")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Seiko")
+                        name.set("SeikoDes")
+                        email.set("seiko_des@outlook.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/qdsfdhvh/compose-imageLoader")
+                    connection.set("scm:git:git://github.com/qdsfdhvh/compose-imageLoader.git")
+                    developerConnection.set("scm:git:git://github.com/qdsfdhvh/compose-imageLoader.git")
+                }
+            }
         }
     }
 }
--- image-loader/build.gradle.kts
+++ image-loader/build.gradle.kts
@@ -1,16 +1,12 @@
-import org.jetbrains.compose.compose
-import java.util.Properties
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
 
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose").version(Versions.compose_jb)
     id("com.android.library")
-    id("maven-publish")
-    id("signing")
+    id("com.vanniktech.maven.publish.base")
 }
-
-group = "io.github.qdsfdhvh"
-version = "1.0.7"
 
 kotlin {
     android {
@@ -87,71 +83,7 @@ android {
     }
 }
 
-ext {
-    val publishPropFile = rootProject.file("publish.properties")
-    if (publishPropFile.exists()) {
-        Properties().apply {
-            load(publishPropFile.inputStream())
-        }.forEach { name, value ->
-            set(name.toString(), value)
-        }
-    } else {
-        set("signing.keyId", System.getenv("SIGNING_KEY_ID"))
-        set("signing.password", System.getenv("SIGNING_PASSWORD"))
-        set("signing.secretKeyRingFile", System.getenv("SIGNING_SECRET_KEY_RING_FILE"))
-        set("ossrhUsername", System.getenv("OSSRH_USERNAME"))
-        set("ossrhPassword", System.getenv("OSSRH_PASSWORD"))
-    }
-}
-
-val javadocJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("javadoc")
-}
-
-publishing {
-    signing {
-        sign(publishing.publications)
-    }
-    repositories {
-        maven {
-            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-            url = if (version.toString().endsWith("SNAPSHOT")) {
-                uri(snapshotsRepoUrl)
-            } else {
-                uri(releasesRepoUrl)
-            }
-            credentials {
-                username = project.ext.get("ossrhUsername").toString()
-                password = project.ext.get("ossrhPassword").toString()
-            }
-        }
-    }
-    publications.withType<MavenPublication> {
-        artifact(javadocJar.get())
-        pom {
-            name.set("compose-imageLoader")
-            description.set("Compose ImageLoader.")
-            url.set("https://github.com/qdsfdhvh/compose-imageLoader")
-
-            licenses {
-                license {
-                    name.set("MIT")
-                    url.set("https://opensource.org/licenses/MIT")
-                }
-            }
-            developers {
-                developer {
-                    id.set("Seiko")
-                    name.set("Seiko Des")
-                    email.set("seiko_des@outlook.com")
-                }
-            }
-            scm {
-                url.set("https://github.com/qdsfdhvh/compose-imageLoader")
-                connection.set("scm:git:git://github.com/qdsfdhvh/compose-imageLoader.git")
-                developerConnection.set("scm:git:git://github.com/qdsfdhvh/compose-imageLoader.git")
-            }
-        }
-    }
+@Suppress("UnstableApiUsage")
+configure<MavenPublishBaseExtension> {
+    configure(KotlinMultiplatform())
 }
