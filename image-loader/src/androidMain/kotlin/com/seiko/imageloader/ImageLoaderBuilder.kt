ceb581c4653a46110a83d720f411b7ce1c67e249
mimao <qiaoxiaoxi621@gmail.com>
2022-08-09 18:29:14 +0800

    add support for base64 image

--- image-loader/src/androidMain/kotlin/com/seiko/imageloader/ImageLoaderBuilder.kt
+++ image-loader/src/androidMain/kotlin/com/seiko/imageloader/ImageLoaderBuilder.kt
@@ -6,23 +6,18 @@ import com.seiko.imageloader.component.decoder.BitmapFactoryDecoder
 import com.seiko.imageloader.component.decoder.GifDecoder
 import com.seiko.imageloader.component.decoder.ImageDecoderDecoder
 import com.seiko.imageloader.component.decoder.SvgDecoder
+import com.seiko.imageloader.component.fetcher.*
 import com.seiko.imageloader.component.fetcher.AssetUriFetcher
-import com.seiko.imageloader.component.fetcher.BitmapFetcher
-import com.seiko.imageloader.component.fetcher.ByteBufferFetcher
 import com.seiko.imageloader.component.fetcher.ContentUriFetcher
-import com.seiko.imageloader.component.fetcher.DrawableFetcher
 import com.seiko.imageloader.component.fetcher.FileFetcher
-import com.seiko.imageloader.component.fetcher.KtorUrlFetcher
 import com.seiko.imageloader.component.fetcher.ResourceUriFetcher
 import com.seiko.imageloader.component.keyer.FileKeyer
 import com.seiko.imageloader.component.keyer.KtorUlKeyer
 import com.seiko.imageloader.component.keyer.UriKeyer
+import com.seiko.imageloader.component.mapper.*
 import com.seiko.imageloader.component.mapper.ByteArrayMapper
-import com.seiko.imageloader.component.mapper.FileUriMapper
-import com.seiko.imageloader.component.mapper.KtorUrlMapper
 import com.seiko.imageloader.component.mapper.ResourceIntMapper
 import com.seiko.imageloader.component.mapper.ResourceUriMapper
-import com.seiko.imageloader.component.mapper.StringMapper
 import com.seiko.imageloader.request.Options
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
@@ -38,6 +33,7 @@ actual class ImageLoaderBuilder constructor(
     actual fun build(): ImageLoader {
         val components = componentBuilder
             // Mappers
+            .add(Base64Mapper())
             .add(KtorUrlMapper())
             .add(StringMapper())
             .add(FileUriMapper())
@@ -49,6 +45,7 @@ actual class ImageLoaderBuilder constructor(
             .add(FileKeyer(true))
             .add(UriKeyer(context))
             // Fetchers
+            .add(Base64Fetcher.Factory())
             .add(KtorUrlFetcher.Factory(httpClient))
             .add(FileFetcher.Factory())
             .add(AssetUriFetcher.Factory(context))
---
+++ image-loader/src/commonMain/kotlin/com/seiko/imageloader/component/fetcher/Base64Fetcher.kt
@@ -0,0 +1,27 @@
+package com.seiko.imageloader.component.fetcher
+
+import com.seiko.imageloader.component.mapper.Base64Image
+import com.seiko.imageloader.request.Options
+import com.seiko.imageloader.util.bufferedSource
+import io.ktor.util.*
+import io.ktor.utils.io.core.*
+import okio.BufferedSource
+
+class Base64Fetcher(
+    private val data: Base64Image,
+) : Fetcher {
+    override suspend fun fetch(): FetchResult {
+        return FetchSourceResult(
+            source = data.content.bufferedSource(),
+            mimeType = data.contentType,
+        )
+    }
+
+    class Factory : Fetcher.Factory {
+        override fun create(data: Any, options: Options): Fetcher? {
+            return if (data is Base64Image) {
+                Base64Fetcher(data)
+            } else null
+        }
+    }
+}
