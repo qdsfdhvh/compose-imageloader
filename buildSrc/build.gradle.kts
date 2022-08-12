9e0cbc7866c35c7abdfef277f3d6577981c79a16
seiko <seiko_des@outlook.com>
2022-08-12 10:02:18 +0800

    AGP to 7.2.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.2.0")
+    implementation("com.android.tools.build:gradle:7.2.2")
     implementation(kotlin("gradle-plugin", version = "1.7.0"))
 }
