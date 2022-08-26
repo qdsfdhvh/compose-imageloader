3137799f8a0f7e9427c40abd54590b7c85d51ded
seiko <seiko_des@outlook.com>
2022-08-26 11:36:01 +0800

    update demo

--- app/common/src/commonMain/kotlin/com/seiko/imageloader/demo/App.kt
+++ app/common/src/commonMain/kotlin/com/seiko/imageloader/demo/App.kt
@@ -2,12 +2,17 @@ package com.seiko.imageloader.demo
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
@@ -66,14 +71,8 @@ private fun ImageList(modifier: Modifier = Modifier) {
         }
     }
 
-    LazyVerticalGrid(GridCells.Fixed(3), modifier) {
-        item {
-            ImageItem("https://pbs.twimg.com/profile_images/1233337597/icon_normal.gif")
-        }
-        item {
-            ImageItem("https://pbs.twimg.com/profile_images/27173262/slam2_normal.gif")
-        }
-        items(images) { image ->
+    LazyColumn(modifier) {
+        itemsGridIndexed(images, 3) { image ->
             ImageItem(image.imageUrl)
         }
     }
@@ -97,6 +96,35 @@ fun ImageItem(url: String) {
                 Text(requestState.error.message ?: "Error")
             }
             ImageRequestState.Success -> Unit
+        }
+    }
+}
+
+
+fun <T> LazyListScope.itemsGridIndexed(
+    data: List<T>,
+    rowSize: Int,
+    itemContent: @Composable BoxScope.(T) -> Unit,
+) {
+    val rows = data.windowed(rowSize, rowSize, true)
+    items(rows) { row ->
+        Column(Modifier.fillParentMaxWidth()) {
+            Row(Modifier.fillMaxWidth()) {
+                val subSize = rowSize - row.size
+                for (i in row.indices) {
+                    val item = row[i]
+                    // val realIndex = i + index * rowSize
+                    Box(modifier = Modifier.weight(1f)) {
+                        itemContent(item)
+                    }
+                }
+
+                if (subSize > 0) {
+                    for (j in 0 until subSize) {
+                        Spacer(modifier = Modifier.weight(1f))
+                    }
+                }
+            }
         }
     }
 }
