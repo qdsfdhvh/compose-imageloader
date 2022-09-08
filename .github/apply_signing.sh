6a0c16c39d36c07120ec3b4f51c7850ebd1835f2
seiko <seiko_des@outlook.com>
2022-09-08 21:10:39 +0800

    update github ci

--- .github/apply_signing.sh
+++
@@ -1,6 +0,0 @@
-echo $SIGNING_KEY | base64 -d > ./key.gpg
-echo "signing.keyId=$SIGNING_KEY_ID
-signing.password=$SIGNING_KEY_PASSWORD
-signing.secretKeyRingFile=./key.gpg
-mavenCentralUsername=$NEXUS_USERNAME
-mavenCentralPassword=$NEXUS_PASSWORD" >publish.properties
