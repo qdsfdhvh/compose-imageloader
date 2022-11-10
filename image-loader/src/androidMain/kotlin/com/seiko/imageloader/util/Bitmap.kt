0ba76cc1ecc08e4dbd5159ba8ebffe0ad7c97462
seiko <seiko_des@outlook.com>
2022-11-10 10:19:46 +0800

    fix ImageConfig crash in android less than api 26

--- image-loader/src/androidMain/kotlin/com/seiko/imageloader/util/Bitmap.kt
+++ image-loader/src/androidMain/kotlin/com/seiko/imageloader/util/Bitmap.kt
@@ -1,9 +1,9 @@
 package com.seiko.imageloader.util
 
-import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.os.Build
 import com.seiko.imageloader.request.Options
+import io.github.aakira.napier.Napier
 
 internal val Bitmap.safeConfig: Bitmap.Config
     get() = config ?: Bitmap.Config.ARGB_8888
@@ -11,12 +11,21 @@ internal val Bitmap.safeConfig: Bitmap.Config
 internal val Bitmap.Config.isHardware: Boolean
     get() = Build.VERSION.SDK_INT >= 26 && this == Bitmap.Config.HARDWARE
 
-@SuppressLint("NewApi")
 internal fun Options.ImageConfig.toBitmapConfig(): Bitmap.Config = when (this) {
     Options.ImageConfig.ALPHA_8 -> Bitmap.Config.ALPHA_8
     Options.ImageConfig.ARGB_8888 -> Bitmap.Config.ARGB_8888
-    Options.ImageConfig.RGBA_F16 -> Bitmap.Config.RGBA_F16
-    Options.ImageConfig.HARDWARE -> Bitmap.Config.HARDWARE
+    Options.ImageConfig.RGBA_F16 -> if (Build.VERSION.SDK_INT >= 26) {
+        Bitmap.Config.RGBA_F16
+    } else {
+        Napier.w { "ImageConfig.RGBA_F16 not support in android less than API 26" }
+        Bitmap.Config.ARGB_8888
+    }
+    Options.ImageConfig.HARDWARE -> if (Build.VERSION.SDK_INT >= 26) {
+        Bitmap.Config.HARDWARE
+    } else {
+        Napier.w { "ImageConfig.HARDWARE not support in android less than API 26" }
+        Bitmap.Config.ARGB_8888
+    }
 }
 
 internal fun Bitmap.Config?.toSoftware(): Bitmap.Config {
