d219b5de74e8848f902cb1c54ce56df03c697880
seiko <seiko_des@outlook.com>
2023-01-05 15:33:07 +0800

    apply spotless

--- app/common/src/commonMain/kotlin/com/seiko/imageloader/demo/CustomKtorUrlFetcher.kt
+++ app/common/src/commonMain/kotlin/com/seiko/imageloader/demo/CustomKtorUrlFetcher.kt
@@ -8,11 +8,11 @@ import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.request.headers
-import io.ktor.http.Url
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.headers
+import io.ktor.http.Url
 
 class CustomKtorUrlFetcher(
     httpUrl: Url,
