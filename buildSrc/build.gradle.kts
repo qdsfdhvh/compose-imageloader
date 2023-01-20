45cba62de7665ca7c954bb873db27e031b5e487e
seiko <seiko_des@outlook.com>
2023-01-20 15:51:23 +0800

    bump Kotlin to 1.8.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:7.3.1")
-    implementation(kotlin("gradle-plugin", version = "1.7.20"))
+    implementation(kotlin("gradle-plugin", version = "1.8.0"))
 }
