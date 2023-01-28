435c00f94f39fa04d4f02d0653a900aa6a57301e
seiko <seiko_des@outlook.com>
2023-01-28 14:36:27 +0800

    add skia default image scope

--- image-loader/src/skiaMain/kotlin/com/seiko/imageloader/component/SetupComponents.skia.kt
+++ image-loader/src/skiaMain/kotlin/com/seiko/imageloader/component/SetupComponents.skia.kt
@@ -4,10 +4,11 @@ import androidx.compose.ui.unit.Density
 import com.seiko.imageloader.component.decoder.GifDecoder
 import com.seiko.imageloader.component.decoder.SkiaImageDecoder
 import com.seiko.imageloader.component.decoder.SvgDecoder
+import com.seiko.imageloader.util.ioDispatcher
 import kotlinx.coroutines.CoroutineScope
 
 fun ComponentRegistryBuilder.setupSkiaComponents(
-    imageScope: CoroutineScope,
+    imageScope: CoroutineScope = CoroutineScope(ioDispatcher),
     density: Density = Density(2f),
 ) {
     // Decoders
