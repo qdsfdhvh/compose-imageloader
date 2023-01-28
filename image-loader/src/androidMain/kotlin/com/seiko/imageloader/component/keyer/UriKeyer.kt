77de5bbb498b676fdc5ed56a3bee324773ce7585
seiko <seiko_des@outlook.com>
2023-01-28 09:47:14 +0800

    fix android resource cache key

--- image-loader/src/androidMain/kotlin/com/seiko/imageloader/component/keyer/UriKeyer.kt
+++ image-loader/src/androidMain/kotlin/com/seiko/imageloader/component/keyer/UriKeyer.kt
@@ -9,12 +9,17 @@ import com.seiko.imageloader.option.Options
 class UriKeyer(private val context: Context) : Keyer {
     override fun key(data: Any, options: Options): String? {
         if (data !is Uri) return null
+
+        if (data.scheme != SCHEME_ANDROID_RESOURCE) return data.toString()
+
+        // android uri is mapper to android.resource://example.package.name/12345678,
+        // but resId is changeable, here is convert resId to entryName.
+        val resId = data.pathSegments.lastOrNull()?.toIntOrNull() ?: return data.toString()
+        val entryName = context.resources.getResourceEntryName(resId)
+        val newUri = data.buildUpon().path(entryName).build()
+
         // 'android.resource' uris can change if night mode is enabled/disabled.
-        return if (data.scheme == SCHEME_ANDROID_RESOURCE) {
-            "$data-${context.resources.configuration.nightMode}"
-        } else {
-            data.toString()
-        }
+        return "$newUri-${context.resources.configuration.nightMode}"
     }
 
     private val Configuration.nightMode: Int
