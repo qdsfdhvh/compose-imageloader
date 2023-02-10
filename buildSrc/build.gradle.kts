23e45995e73a6ec9bb11eb331075dcea4a45034f
seiko <seiko_des@outlook.com>
2023-02-10 12:34:03 +0800

    bump AGP to 7.4.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.4.0")
+    implementation("com.android.tools.build:gradle:7.4.1")
     implementation(kotlin("gradle-plugin", version = "1.8.0"))
 }
