6d78f3ec57fb848b920401f94fa20eb7267f3a8d
seiko <seiko_des@outlook.com>
2023-02-10 12:09:02 +0800

    bump compose-jb to 1.3.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -22,7 +22,7 @@ object Versions {
 
     const val spotless = "6.14.1"
     const val ktlint = "0.48.2"
-    const val compose_jb = "1.3.0-rc05"
+    const val compose_jb = "1.3.0"
     const val multiplatformResources = "0.20.1"
     const val ktor = "2.2.1"
     const val okio = "3.3.0"
