508d00ec6a586f3e0f0944d51f07f939fd99093e
seiko <seiko_des@outlook.com>
2023-03-01 12:58:46 +0800

    support ios safe area

--- app/ios-combine/src/commonMain/kotlin/com/seiko/imageloader/demo/Main.kt
+++ app/ios-combine/src/commonMain/kotlin/com/seiko/imageloader/demo/Main.kt
@@ -1,6 +1,11 @@
 package com.seiko.imageloader.demo
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 import com.seiko.imageloader.ImageLoader
 import com.seiko.imageloader.LocalImageLoader
@@ -13,44 +18,59 @@ import okio.Path.Companion.toPath
 import platform.Foundation.NSCachesDirectory
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSUserDomainMask
+import platform.UIKit.UILayoutGuide
 import platform.UIKit.UIViewController
-
+import platform.UIKit.UIWindow
+import platform.UIKit.accessibilityFrame
+import platform.UIKit.safeAreaLayoutGuide
 
 @Suppress("FunctionName")
-fun MainViewController(): UIViewController = ComposeUIViewController {
+fun MainViewController(window: UIWindow): UIViewController = ComposeUIViewController {
     CompositionLocalProvider(
-       LocalImageLoader provides generateImageLoader(),
-       LocalResLoader provides ResLoader(),
+        LocalImageLoader provides generateImageLoader(),
+        LocalResLoader provides ResLoader(),
     ) {
-        App()
+        Column {
+            App(
+                modifier = Modifier
+                    .windowInsetsPadding(window.safeAreaLayoutGuide.toWindowInsets()),
+            )
+        }
     }
 }
 
+private fun UILayoutGuide.toWindowInsets() = WindowInsets(
+    top = topAnchor.accessibilityFrame.size.dp,
+    // bottom = bottomAnchor.accessibilityFrame.size.dp,
+    // left = leftAnchor.accessibilityFrame.size.dp,
+    // right = rightAnchor.accessibilityFrame.size.dp,
+)
+
 private fun generateImageLoader(): ImageLoader {
-   return ImageLoader {
-       commonConfig()
-       components {
-           setupDefaultComponents(imageScope)
-       }
-       interceptor {
-           memoryCacheConfig {
-               // Set the max size to 25% of the app's available memory.
-               maxSizePercent(0.25)
-           }
-           diskCacheConfig {
-               directory(getCacheDir().toPath().resolve("image_cache"))
-               maxSizeBytes(512L * 1024 * 1024) // 512MB
-           }
-       }
-   }
+    return ImageLoader {
+        commonConfig()
+        components {
+            setupDefaultComponents(imageScope)
+        }
+        interceptor {
+            memoryCacheConfig {
+                // Set the max size to 25% of the app's available memory.
+                maxSizePercent(0.25)
+            }
+            diskCacheConfig {
+                directory(getCacheDir().toPath().resolve("image_cache"))
+                maxSizeBytes(512L * 1024 * 1024) // 512MB
+            }
+        }
+    }
 }
 
 private fun getCacheDir(): String {
-   return NSFileManager.defaultManager.URLForDirectory(
-       NSCachesDirectory,
-       NSUserDomainMask,
-       null,
-       true,
-       null,
-   )!!.path.orEmpty()
+    return NSFileManager.defaultManager.URLForDirectory(
+        NSCachesDirectory,
+        NSUserDomainMask,
+        null,
+        true,
+        null,
+    )!!.path.orEmpty()
 }
--- app/ios/iosApp/iOSApp.swift
+++ app/ios/iosApp/iOSApp.swift
@@ -3,12 +3,11 @@ import combine
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let mainViewController = MainKt.MainViewController()
-        window?.rootViewController = mainViewController
-        window?.makeKeyAndVisible()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let mainViewController = MainKt.MainViewController(window: window)
+        window.rootViewController = mainViewController
+        window.makeKeyAndVisible()
         return true
     }
 }
