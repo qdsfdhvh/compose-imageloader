9aba342f8e5f3771e988497dccf65a9e9771a549
seiko <seiko_des@outlook.com>
2023-04-20 17:28:26 +0800

    update build gradle

--- extension/blur/build.gradle.kts
+++ extension/blur/build.gradle.kts
@@ -1,60 +1,22 @@
 import com.vanniktech.maven.publish.KotlinMultiplatform
 
 plugins {
-    kotlin("multiplatform")
-    id("org.jetbrains.compose")
-    id("com.android.library")
+    id("project-kmp")
     id("com.vanniktech.maven.publish.base")
 }
 
 kotlin {
-    android {
-        publishLibraryVariants("debug", "release")
-    }
-    jvm("desktop") {
-        compilations.all {
-            kotlinOptions.jvmTarget = Versions.Java.jvmTarget
-        }
-    }
-    ios()
-    iosSimulatorArm64()
-    macosX64()
-    macosArm64()
-    js(IR) {
-        browser()
-        nodejs()
-    }
     sourceSets {
-        val commonMain by getting {
+        commonMain {
             dependencies {
                 implementation(projects.imageLoader)
             }
         }
+        @Suppress("UNUSED_VARIABLE")
         val androidMain by getting {
             dependencies {
                 implementation("com.github.android:renderscript-intrinsics-replacement-toolkit:9a70eae6f1")
             }
-        }
-        val skiaMain by creating {
-            dependsOn(commonMain)
-        }
-        val desktopMain by getting {
-            dependsOn(skiaMain)
-        }
-        val iosMain by getting {
-            dependsOn(skiaMain)
-        }
-        val iosSimulatorArm64Main by getting {
-            dependsOn(skiaMain)
-        }
-        val macosX64Main by getting {
-            dependsOn(skiaMain)
-        }
-        val macosArm64Main by getting {
-            dependsOn(skiaMain)
-        }
-        val jsMain by getting {
-            dependsOn(skiaMain)
         }
     }
 }
