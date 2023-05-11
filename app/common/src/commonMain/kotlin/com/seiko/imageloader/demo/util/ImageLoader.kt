9c0e5480e5e8ecc5b4c543ac415009a46814c5a6
seiko <seiko_des@outlook.com>
2023-05-11 14:47:14 +0800

    migrate napier to kermit

--- app/common/src/commonMain/kotlin/com/seiko/imageloader/demo/util/ImageLoader.kt
+++ app/common/src/commonMain/kotlin/com/seiko/imageloader/demo/util/ImageLoader.kt
@@ -3,16 +3,50 @@ package com.seiko.imageloader.demo.util
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.painter.Painter
+import co.touchlab.kermit.Severity
 import com.seiko.imageloader.ImageLoaderConfigBuilder
 import com.seiko.imageloader.intercept.BlurInterceptor
 import com.seiko.imageloader.intercept.Interceptor
 import com.seiko.imageloader.model.ImageResult
 import com.seiko.imageloader.model.NullRequestData
-import com.seiko.imageloader.util.DebugLogger
 import com.seiko.imageloader.util.LogPriority
+import com.seiko.imageloader.util.Logger
+import co.touchlab.kermit.Logger as DebugLogger
 
 fun ImageLoaderConfigBuilder.commonConfig() {
-    logger = DebugLogger(LogPriority.VERBOSE)
+    logger = object : Logger {
+        override fun log(
+            priority: LogPriority,
+            tag: String,
+            data: Any?,
+            throwable: Throwable?,
+            message: String
+        ) {
+            DebugLogger.log(
+                severity = when (priority) {
+                    LogPriority.VERBOSE -> Severity.Verbose
+                    LogPriority.DEBUG -> Severity.Debug
+                    LogPriority.INFO -> Severity.Info
+                    LogPriority.WARN -> Severity.Warn
+                    LogPriority.ERROR -> Severity.Error
+                    LogPriority.ASSERT -> Severity.Assert
+                },
+                tag = tag,
+                throwable = throwable,
+                message = buildString {
+                    if (data != null) {
+                        append("[image data] ")
+                        append(data.toString().take(100))
+                        append('\n')
+                    }
+                    append("[message] ")
+                    append(message)
+                }
+            )
+        }
+
+        override fun isLoggable(priority: LogPriority) = priority >= LogPriority.DEBUG
+    }
     interceptor {
         addInterceptor(BlurInterceptor())
     }
