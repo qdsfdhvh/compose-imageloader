b6f7ad75f5980810d2e94e87f0c8a0e372598a0c
seiko <seiko_des@outlook.com>
2023-05-16 09:54:16 +0800

    set skia default imageScope

--- image-loader/src/darwinMain/singleton/com/seiko/imageloader/component/SetupDefaultComponents.darwin.kt
+++ image-loader/src/darwinMain/singleton/com/seiko/imageloader/component/SetupDefaultComponents.darwin.kt
@@ -2,12 +2,12 @@ package com.seiko.imageloader.component
 
 import androidx.compose.ui.unit.Density
 import com.seiko.imageloader.util.httpEngine
-import com.seiko.imageloader.util.ioDispatcher
 import io.ktor.client.HttpClient
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 fun ComponentRegistryBuilder.setupDefaultComponents(
-    imageScope: CoroutineScope = CoroutineScope(ioDispatcher),
+    imageScope: CoroutineScope = CoroutineScope(Dispatchers.Main),
     density: Density = Density(2f),
     httpClient: () -> HttpClient = { HttpClient(httpEngine) },
 ) {
--- image-loader/src/skiaMain/kotlin/com/seiko/imageloader/component/SetupComponents.skia.kt
+++ image-loader/src/skiaMain/kotlin/com/seiko/imageloader/component/SetupComponents.skia.kt
@@ -4,11 +4,11 @@ import androidx.compose.ui.unit.Density
 import com.seiko.imageloader.component.decoder.GifDecoder
 import com.seiko.imageloader.component.decoder.SkiaImageDecoder
 import com.seiko.imageloader.component.decoder.SvgDecoder
-import com.seiko.imageloader.util.ioDispatcher
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 fun ComponentRegistryBuilder.setupSkiaComponents(
-    imageScope: CoroutineScope = CoroutineScope(ioDispatcher),
+    imageScope: CoroutineScope = CoroutineScope(Dispatchers.Main),
     density: Density = Density(2f),
 ) {
     // Decoders
