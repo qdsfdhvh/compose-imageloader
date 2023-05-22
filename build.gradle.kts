55a7fa14cb2d6cf43f8917dc4eedac722a5ab348
seiko <seiko_des@outlook.com>
2023-05-22 10:58:22 +0800

    use compose-1.4.0 for no jsWasm

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,4 +68,20 @@ allprojects {
             }
         }
     }
+
+    // For wasm
+    // https://github.com/Kotlin/kotlin-wasm-examples/blob/main/compose-imageviewer/build.gradle.kts
+    configurations.all {
+        val conf = this
+        // Currently it's necessary to make the android build work properly
+        conf.resolutionStrategy.eachDependency {
+            val isWasm = conf.name.contains("wasm", true)
+            val isJs = conf.name.contains("js", true)
+            val isComposeGroup = requested.module.group.startsWith("org.jetbrains.compose")
+            val isComposeCompiler = requested.module.group.startsWith("org.jetbrains.compose.compiler")
+            if (isComposeGroup && !isComposeCompiler && !isWasm && !isJs) {
+                useVersion("1.4.0")
+            }
+        }
+    }
 }
