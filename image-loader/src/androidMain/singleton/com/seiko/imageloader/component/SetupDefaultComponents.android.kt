b532cf12230f0e8e8838955dd3433f0cc38fd10a
seiko <seiko_des@outlook.com>
2023-06-03 20:24:33 +0800

    cleanup code

--- image-loader/src/androidMain/singleton/com/seiko/imageloader/component/SetupDefaultComponents.android.kt
+++ image-loader/src/androidMain/singleton/com/seiko/imageloader/component/SetupDefaultComponents.android.kt
@@ -2,13 +2,14 @@ package com.seiko.imageloader.component
 
 import android.content.Context
 import androidx.compose.ui.unit.Density
+import com.seiko.imageloader.component.decoder.BitmapFactoryDecoder
 import com.seiko.imageloader.util.httpEngine
 import io.ktor.client.HttpClient
 
 fun ComponentRegistryBuilder.setupDefaultComponents(
     context: Context? = null,
     density: Density? = context?.let { Density(it) },
-    maxImageSize: Int = 4096,
+    maxImageSize: Int = BitmapFactoryDecoder.DEFAULT_MAX_PARALLELISM,
     httpClient: () -> HttpClient = { HttpClient(httpEngine) },
 ) {
     setupKtorComponents(httpClient)
