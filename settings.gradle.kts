eefaeadd2681b56581c679175a26e137b686f9b7
seiko <seiko_des@outlook.com>
2023-06-27 15:28:02 +0800

    bump ktor to 2.3.2-eap-701

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven("https://repo1.maven.org/maven2")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap") // TODO remove this line
         maven("https://jitpack.io")
     }
 }
