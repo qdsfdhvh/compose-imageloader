35aac62b37522e9ce2001af6a93b2ec4dd874a44
seiko <seiko_des@outlook.com>
2023-06-27 16:16:49 +0800

    fix build

--- app/intellij-plugin/build.gradle.kts
+++ app/intellij-plugin/build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
     google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap") // TODO remove this line
 }
 
 java {
