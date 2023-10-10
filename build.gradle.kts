697303fdfa20d6b2e906b6f5df92592fb808e501
seiko <seiko_des@outlook.com>
2023-10-10 09:34:39 +0800

    version to 1.6.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,7 +90,7 @@ object ProjectVersion {
     private const val monir = "6"
 
     // backwards compatible bug fixes
-    private const val path = "7"
+    private const val path = "8"
     const val version = "$major.$monir.$path"
 }
 
