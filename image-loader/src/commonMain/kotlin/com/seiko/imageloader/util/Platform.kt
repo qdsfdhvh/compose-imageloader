81c68ae663fe4eb6f9234f98eda0fb1c26e1fd5e
seiko <seiko_des@outlook.com>
2023-11-14 09:26:49 +0800

    revert default ktor engine

--- image-loader/src/commonMain/kotlin/com/seiko/imageloader/util/Platform.kt
+++ image-loader/src/commonMain/kotlin/com/seiko/imageloader/util/Platform.kt
@@ -1,6 +1,7 @@
 package com.seiko.imageloader.util
 
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
 import io.ktor.utils.io.ByteReadChannel
 import kotlinx.coroutines.CoroutineDispatcher
 import okio.BufferedSource
@@ -16,7 +17,9 @@ internal expect suspend fun ByteReadChannel.source(): BufferedSource
 
 internal expect val ioDispatcher: CoroutineDispatcher
 
+internal expect val httpEngine: HttpClientEngine
+
 internal expect val defaultFileSystem: FileSystem?
 
 internal val httpEngineFactory: () -> HttpClient
-    get() = { HttpClient() }
+    get() = { HttpClient(httpEngine) }
