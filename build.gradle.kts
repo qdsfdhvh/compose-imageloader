ee65995804a0b5fca1c5542a0db65809d3bc3421
seiko <seiko_des@outlook.com>
2024-02-14 17:10:45 +0800

    version to 1.7.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ object ProjectVersion {
     private const val monir = "7"
 
     // backwards compatible bug fixes
-    private const val path = "4"
+    private const val path = "5"
     const val version = "$major.$monir.$path"
 }
 
