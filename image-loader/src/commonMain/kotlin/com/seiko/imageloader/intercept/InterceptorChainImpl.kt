34042d202c905c2aba2b85bd242c90036940cf72
seiko <seiko_des@outlook.com>
2024-02-18 17:05:59 +0800

    private class InterceptorChainImpl & InterceptorChainHelper

--- image-loader/src/commonMain/kotlin/com/seiko/imageloader/intercept/InterceptorChainImpl.kt
+++ image-loader/src/commonMain/kotlin/com/seiko/imageloader/intercept/InterceptorChainImpl.kt
@@ -6,21 +6,17 @@ import com.seiko.imageloader.model.ImageAction
 import com.seiko.imageloader.model.ImageRequest
 import com.seiko.imageloader.model.ImageResult
 import com.seiko.imageloader.option.Options
+import com.seiko.imageloader.option.takeFrom
 import com.seiko.imageloader.util.Logger
 import kotlinx.coroutines.flow.FlowCollector
 
-internal class InterceptorChainImpl(
-    private val helper: InterceptorChainHelper,
-    override val request: ImageRequest,
-    private val index: Int,
-) : Interceptor.Chain {
-
-    constructor(
-        initialRequest: ImageRequest,
-        initialOptions: Options,
-        config: ImageLoaderConfig,
-        flowCollector: FlowCollector<ImageAction>,
-    ) : this(
+fun createInterceptorChain(
+    initialRequest: ImageRequest,
+    initialOptions: Options,
+    config: ImageLoaderConfig,
+    flowCollector: FlowCollector<ImageAction>,
+): Interceptor.Chain {
+    return InterceptorChainImpl(
         helper = InterceptorChainHelper(
             initialImageRequest = initialRequest,
             initialOptions = initialOptions,
@@ -30,6 +26,13 @@ internal class InterceptorChainImpl(
         request = initialRequest,
         index = 0,
     )
+}
+
+private class InterceptorChainImpl(
+    private val helper: InterceptorChainHelper,
+    override val request: ImageRequest,
+    private val index: Int,
+) : Interceptor.Chain {
 
     private fun copy(index: Int, request: ImageRequest) = InterceptorChainImpl(
         helper = helper,
@@ -54,5 +57,34 @@ internal class InterceptorChainImpl(
 
     override suspend fun emit(action: ImageAction) {
         helper.emit(action)
+    }
+}
+
+private class InterceptorChainHelper(
+    initialImageRequest: ImageRequest,
+    private val initialOptions: Options,
+    private val config: ImageLoaderConfig,
+    private val flowCollector: FlowCollector<ImageAction>,
+) {
+    val logger get() = config.logger
+
+    val interceptors by lazy {
+        initialImageRequest.interceptors?.plus(config.interceptors.list)
+            ?: config.interceptors.list
+    }
+
+    val components by lazy {
+        initialImageRequest.components?.merge(config.componentRegistry)
+            ?: config.componentRegistry
+    }
+
+    fun getOptions(request: ImageRequest): Options {
+        return Options(initialOptions) {
+            takeFrom(request)
+        }
+    }
+
+    suspend fun emit(action: ImageAction) {
+        flowCollector.emit(action)
     }
 }
