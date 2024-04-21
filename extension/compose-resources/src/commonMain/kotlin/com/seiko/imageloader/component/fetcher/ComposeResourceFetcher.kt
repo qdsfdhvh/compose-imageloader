747d4ec51810c39e8e82b6fbbf3accbaeea3553f
seiko <seiko_des@outlook.com>
2024-04-21 19:42:19 +0800

    compose-multiplatform to 1.6.10-beta01

--- extension/compose-resources/src/commonMain/kotlin/com/seiko/imageloader/component/fetcher/ComposeResourceFetcher.kt
+++ extension/compose-resources/src/commonMain/kotlin/com/seiko/imageloader/component/fetcher/ComposeResourceFetcher.kt
@@ -7,11 +7,11 @@ import okio.Buffer
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.InternalResourceApi
-import org.jetbrains.compose.resources.getPathByEnvironment
+import org.jetbrains.compose.resources.getResourceItemByEnvironment
 import org.jetbrains.compose.resources.getSystemEnvironment
 import org.jetbrains.compose.resources.readResourceBytes
 
-@OptIn(ExperimentalResourceApi::class)
+@OptIn(ExperimentalResourceApi::class, InternalResourceApi::class)
 class ComposeResourceFetcher {
     class Factory : Fetcher.Factory {
         override fun create(data: Any, options: Options): Fetcher? {
@@ -25,9 +25,8 @@ class ComposeResourceFetcher {
     private class DrawableResourceFetcher(
         private val resource: DrawableResource,
     ) : Fetcher {
-        @OptIn(InternalResourceApi::class)
         override suspend fun fetch(): FetchResult {
-            val path = resource.getPathByEnvironment(getSystemEnvironment())
+            val path = resource.getResourceItemByEnvironment(getSystemEnvironment()).path
             return FetchResult.OfSource(
                 source = Buffer().apply {
                     write(readResourceBytes(path))
