ddd2b4dd36aac1f8bf0ae9d2b9d05c33a3c7c555
seiko <seiko_des@outlook.com>
2024-04-21 19:41:54 +0800

    poko to 0.16.0-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,8 @@ pluginManagement {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        // FIXME: remove this maven
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
     includeBuild("build-logic")
 }
@@ -17,6 +19,8 @@ dependencyResolutionManagement {
         maven("https://jitpack.io")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
+        // FIXME: remove this maven
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
 }
 
