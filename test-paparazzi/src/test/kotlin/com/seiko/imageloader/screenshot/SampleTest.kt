0dc01e021e0dc3fbb9b727fa8f297dbf38d5cb22
seiko <seiko_des@outlook.com>
2024-07-03 11:38:56 +0800

    apply spotless

--- test-paparazzi/src/test/kotlin/com/seiko/imageloader/screenshot/SampleTest.kt
+++ test-paparazzi/src/test/kotlin/com/seiko/imageloader/screenshot/SampleTest.kt
@@ -3,7 +3,6 @@ package com.seiko.imageloader.screenshot
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Modifier
