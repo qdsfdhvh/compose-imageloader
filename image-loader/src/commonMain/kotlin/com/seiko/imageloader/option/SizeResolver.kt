1e2547f1659296a6c1e62285809b3922785865ad
seiko <seiko_des@outlook.com>
2024-10-09 20:32:04 +0800

    add size suffix for url keyer

--- image-loader/src/commonMain/kotlin/com/seiko/imageloader/option/SizeResolver.kt
+++ image-loader/src/commonMain/kotlin/com/seiko/imageloader/option/SizeResolver.kt
@@ -1,6 +1,9 @@
 package com.seiko.imageloader.option
 
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 
 interface SizeResolver {
     suspend fun size(): Size
@@ -15,6 +18,20 @@ fun SizeResolver(block: suspend () -> Size) = object : SizeResolver {
 }
 
 fun SizeResolver(size: Size): SizeResolver = RealSizeResolver(size)
+
+fun SizeResolver(size: Float): SizeResolver = RealSizeResolver(Size(size, size))
+
+fun SizeResolver(width: Float, height: Float): SizeResolver = RealSizeResolver(Size(width, height))
+
+fun SizeResolver(size: Int): SizeResolver = RealSizeResolver(Size(size.toFloat(), size.toFloat()))
+
+fun SizeResolver(width: Int, height: Int): SizeResolver = RealSizeResolver(Size(width.toFloat(), height.toFloat()))
+
+fun SizeResolver(density: Density, size: DpSize): SizeResolver = with(density) { RealSizeResolver(size.toSize()) }
+
+fun SizeResolver(density: Density, size: Dp): SizeResolver = with(density) { RealSizeResolver(Size(size.toPx(), size.toPx())) }
+
+fun SizeResolver(density: Density, width: Dp, height: Dp): SizeResolver = with(density) { RealSizeResolver(Size(width.toPx(), height.toPx())) }
 
 private class RealSizeResolver(private val size: Size) : SizeResolver {
     override suspend fun size(): Size = size
