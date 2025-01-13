19c5d05f4fb519d9aa07719307371d0b394cd02d
seiko <seiko_des@outlook.com>
2025-01-13 16:06:29 +0800

    replace to com.gradle.develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version "3.18.2"
+    id("com.gradle.develocity") version "3.18.2"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
