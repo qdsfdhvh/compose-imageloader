9f1b76bc01bab493d4be4727e47dfe6cc4db01db
seiko <seiko_des@outlook.com>
2025-01-15 13:15:29 +0800

    apply spotless

--- image-loader/src/androidMain/kotlin/com/seiko/imageloader/cache/disk/DiskCacheBuilder.android.kt
+++ image-loader/src/androidMain/kotlin/com/seiko/imageloader/cache/disk/DiskCacheBuilder.android.kt
@@ -5,10 +5,12 @@ import okio.FileSystem
 import okio.Path
 
 internal actual fun FileSystem.remainingFreeSpaceBytes(directory: Path): Long {
-    val stats = StatFs(directory.toFile().apply {
-        if (!exists()) {
-            mkdir()
-        }
-    }.absolutePath)
+    val stats = StatFs(
+        directory.toFile().apply {
+            if (!exists()) {
+                mkdir()
+            }
+        }.absolutePath,
+    )
     return stats.blockCountLong * stats.blockSizeLong
 }
--- image-loader/src/appleMain/kotlin/com/seiko/imageloader/cache/disk/DiskCacheBuilder.apple.kt
+++ image-loader/src/appleMain/kotlin/com/seiko/imageloader/cache/disk/DiskCacheBuilder.apple.kt
@@ -9,7 +9,7 @@ import platform.Foundation.fileSize
 
 @OptIn(ExperimentalForeignApi::class)
 internal actual fun FileSystem.remainingFreeSpaceBytes(directory: Path): Long {
-    val fileSize = NSFileManager.defaultManager.attributesOfFileSystemForPath(directory.toString(),null) as? NSDictionary
+    val fileSize = NSFileManager.defaultManager.attributesOfFileSystemForPath(directory.toString(), null) as? NSDictionary
     if (fileSize != null) {
         return fileSize.fileSize().toLong()
     }
